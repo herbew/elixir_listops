3de74c04d98e09b8bef7ad5eaf3122a47436fd0b
Herbew <herbew@gmail.com>
2022-04-29 13:06:28 +0700

    count test repairing

--- lib/listops.ex
+++ lib/listops.ex
@@ -1,9 +1,11 @@
 defmodule ListOps do
-	@doc "Please don't use any external modules (especially List) in your " <> \
-	"implementation. The point of this exercise is " <> \ 
-	"to create these basic functions yourself.\n\n" <> \
-	"Note that `++` is a function from an external module (Kernel, which is " <> \
-	"automatically imported) and so shouldn't be used either."
+	@doc"""
+    Please don't use any external modules (especially List) in your 
+    implementation. The point of this exercise is 
+    to create these basic functions yourself.
+    Note that `++` is a function from an external module (Kernel, which is 
+    automatically imported) and so shouldn't be used either.
+    """
 	
 	# count of list
 	@spec count(list) :: non_neg_integer
--- test/listops_test.exs
+++ test/listops_test.exs
@@ -4,18 +4,18 @@ defmodule ListOpsTest do
 	describe "count" do
 	    # @tag :pending
 	    test "empty list" do
-	      assert ListOpsTest.count([]) == 0
+	      assert ListOps.count([]) == 0
 	    end#
 	
 	    @tag :pending
 	    test "normal list" do
-	      assert ListOpsTest.count([1, 2, 3, 4]) == 4
+	      assert ListOps.count([1, 2, 3, 4]) == 4
 	    end
 	
 	    @tag :pending
 	    @tag :slow
 	    test "huge list" do
-	      assert ListOpsTest.count(Enum.to_list(1..1_000_000)) == 1_000_000
+	      assert ListOps.count(Enum.to_list(1..1_000_000)) == 1_000_000
 	    end
 	end #describe "count" do
 	
